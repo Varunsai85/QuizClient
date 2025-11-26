8ca56a7a8d45205f97ddf6fb764aa612d5feefe8
Varunsai85 <varunsainadiminti@gmail.com>
2025-11-26 16:11:41 +0530

    Implemented change scene method to navigate between pages

--- src/main/java/org/varun/quizclient/HelloApplication.java
+++ src/main/java/org/varun/quizclient/HelloApplication.java
@@ -2,18 +2,27 @@ package org.varun.quizclient;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+    private static Stage stg;
     @Override
     public void start(Stage stage) throws IOException {
+        stg=stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException{
+        Parent parent=FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxml)));
+        stg.getScene().setRoot(parent);
     }
 }
