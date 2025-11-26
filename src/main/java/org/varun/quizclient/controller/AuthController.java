3fd3aae2ee638f299c41c5005236f720d9746fc2
Varunsai85 <varunsainadiminti@gmail.com>
2025-11-26 16:12:45 +0530

    Implemented navigation methods

--- src/main/java/org/varun/quizclient/controller/AuthController.java
+++ src/main/java/org/varun/quizclient/controller/AuthController.java
@@ -1,28 +1,39 @@
 package org.varun.quizclient.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import org.varun.quizclient.HelloApplication;
 import org.varun.quizclient.service.AuthService;
 
-public class LoginController {
+import java.io.IOException;
+
+public class AuthController {
     @FXML
     private Label titleLabel;
     @FXML
     private TextField logInField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField emailField;
     @FXML
     private PasswordField passwordField;
     @FXML
     private Button logInButton;
     @FXML
     private Label errorLabel;
+    @FXML
+    private Button signUpButton;
+    @FXML
+    private Hyperlink logInSwitch;
+    @FXML
+    private Hyperlink signUpSwitch;
 
     private final AuthService authService=new AuthService();
+    private final HelloApplication ha=new HelloApplication();
 
     @FXML
-    public void onLoginClick() {
+    public void onLogInClick() {
         try {
             String message= authService.login(logInField.getText(),passwordField.getText());
             errorLabel.setStyle("-fx-text-fill: green");
@@ -31,5 +42,15 @@ public class LoginController {
             errorLabel.setStyle("-fx-text-fill: red");
             errorLabel.setText(e.getMessage());
         }
+    }
+
+    @FXML
+    public void signUpNavigate() throws IOException {
+        ha.changeScene("views/signup-view.fxml");
+    }
+
+    @FXML
+    public void logInNavigate() throws IOException {
+        ha.changeScene("views/login-view.fxml");
     }
 }
